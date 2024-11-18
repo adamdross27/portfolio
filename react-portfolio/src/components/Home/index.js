6f08806f16b1ed2104aa864a3be51a14795f32b1
adamdross27 <adamross387@gmail.com>
2024-11-18 23:59:01 +1300

    added to home page, animations, hover, to animate logo on right side next

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,49 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['d', 'a', 'm'];
+    const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', '', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r'];
+
+    useEffect(() => {
+        const animationTimeout = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+
+        return () => clearTimeout(animationTimeout);
+    }, []);
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15} // Stagger starts from 15
+                    />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={18} // Stagger starts from 18
+                    />
+                </h1>
+                <h2> Full-Stack Developer</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
