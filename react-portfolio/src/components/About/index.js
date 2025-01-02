1d54bd3f378d933256fec034fdadda407392f78d
adamdross27 <adamross387@gmail.com>
2025-01-02 13:50:33 +1300

    changes to layout of About page

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -30,16 +30,38 @@ const About = () => {
                     <h2>
                         <AnimatedLetters
                             letterClass={letterClass}
-                            strArray={'Education'.split('')}
-                            idx={15}
+                            strArray={'Software Development Experience'.split('')}
+                            idx={3}
                         />
                     </h2>
                     <p>
-                        I have graduated from Auckland University of Technology with a <i>Bachelor of Computer and Information Sciences</i>,
-                        majoring in Software Development, and minoring in Networks & Cybersecurity. <b>Graduating with an A- overall (7.16/9)
-                        GPA </b> is a great achievement for me and it shows the consistent hard work I have put into my degree.
+                        In 2024, I led a team of five students from AUT on a Research & Development project for <b>CAITO AI</b>, a company 
+                        specializing in transforming unstructured data into actionable insights for clients. <b>As the project leader, I was 
+                        responsible</b> for outlining the project <b>management methodology</b>, and we decided to implement the Agile Scrum methodology, 
+                        completing eight development sprints. Our client required us to tailor the project for the Government Officers of the 
+                        Technical and Vocational Education Training (TVET) sector in South East Asia, specifically The Philippines. 
+                        <br /><br />
+                        We created the <b>CAITO Workforce Intelligence System</b> (CAITO WIS), a platform that allows users to web scrape or insert 
+                        data and use generative AI to provide technical insights. This enables CAITO's clients to make informed, data-driven 
+                        decisions. The system allows users to view and customize data displays, and generate in-depth AI analyses from graphs 
+                        to improve decision-making skills. We also built a custom job scraper that searches JobStreet.ph for real-time job postings,
+                        which are collected into a Firebase database. Additionally, our Skill Extractor enables users to provide a job description,
+                        and the AI generates key skills related to the description along with a confidence level. 
+                        <br /><br />
+                        The combination of the job scraper and the skill extractor helps users identify in-demand jobs and the skills needed for 
+                        those roles. This empowers government officers to promote industries based on current job market trends. 
+                        <br /><br />
+                        The CAITO WIS was developed using the <b>FERN stack</b> (Firebase, Express.js, React.js, Node.js), providing a full-stack 
+                        solution. Our front-end was built with React, the backend utilized Express.js and Node.js, and our database was hosted 
+                        on Firebase. 
+                        <br /><br />
+                        We successfully delivered all the proposed deliverables and achieved remarkable results. The web app reduced manual 
+                        reporting time by 30% and improved data retrieval speed by 25%. The integrated AI analysis delivered real-time data-driven
+                        insights, enabling the creation of custom dashboards that improved decision-making speed by 40%. This, in turn, allowed 
+                        for proactive workforce planning opportunities. Our work led to a 20% increase in stakeholder satisfaction, as we exceeded 
+                        their expectations.
                     </p>
-
+                    
                     <h2>
                         <AnimatedLetters
                             letterClass={letterClass}
@@ -64,39 +86,18 @@ const About = () => {
                         </ul>                    
                     </p>
 
+
                     <h2>
                         <AnimatedLetters
                             letterClass={letterClass}
-                            strArray={'Software Development Experience'.split('')}
-                            idx={3}
+                            strArray={'Education'.split('')}
+                            idx={15}
                         />
                     </h2>
                     <p>
-                        In 2024, I led a team of five students from AUT on a Research & Development project for CAITO AI, a company 
-                        specializing in transforming unstructured data into actionable insights for clients. As the project leader, I was 
-                        responsible for outlining the project management methodology, and we decided to implement the Agile Scrum methodology, 
-                        completing eight development sprints. Our client required us to tailor the project for the Government Officers of the 
-                        Technical and Vocational Education Training (TVET) sector in South East Asia, specifically The Philippines. 
-                        <br /><br />
-                        We created the CAITO Workforce Intelligence System (CAITO WIS), a platform that allows users to web scrape or insert 
-                        data and use generative AI to provide technical insights. This enables CAITO's clients to make informed, data-driven 
-                        decisions. The system allows users to view and customize data displays, and generate in-depth AI analyses from graphs 
-                        to improve decision-making skills. We also built a custom job scraper that searches JobStreet.ph for real-time job postings,
-                        which are collected into a Firebase database. Additionally, our Skill Extractor enables users to provide a job description,
-                        and the AI generates key skills related to the description along with a confidence level. 
-                        <br /><br />
-                        The combination of the job scraper and the skill extractor helps users identify in-demand jobs and the skills needed for 
-                        those roles. This empowers government officers to promote industries based on current job market trends. 
-                        <br /><br />
-                        The CAITO WIS was developed using the FERN stack (Firebase, Express.js, React.js, Node.js), providing a full-stack 
-                        solution. Our front-end was built with React, the backend utilized Express.js and Node.js, and our database was hosted 
-                        on Firebase. 
-                        <br /><br />
-                        We successfully delivered all the proposed deliverables and achieved remarkable results. The web app reduced manual 
-                        reporting time by 30% and improved data retrieval speed by 25%. The integrated AI analysis delivered real-time data-driven
-                        insights, enabling the creation of custom dashboards that improved decision-making speed by 40%. This, in turn, allowed 
-                        for proactive workforce planning opportunities. Our work led to a 20% increase in stakeholder satisfaction, as we exceeded 
-                        their expectations.
+                        I have graduated from Auckland University of Technology with a <b>Bachelor of Computer and Information Sciences</b>,
+                        <i> majoring in Software Development, and minoring in Networks & Cybersecurity.</i> <b>Graduating with an A- overall (7.16/9)
+                        GPA </b> is a great achievement for me and it shows the consistent hard work I have put into my degree.
                     </p>
 
                     <h2>
